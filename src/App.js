3445adfd136debb258a075132ea34c56ce4f0811
julou <louis.jungo@gmail.com>
2020-01-14 17:22:18 +0100

    added scroll snapping

--- src/App.js
+++ src/App.js
@@ -6,18 +6,18 @@ function App() {
   return (
     <div className="App">
       <Navigation />
-      <div id="first" className="section first">
-        <h1>1st section</h1>
-      </div>
-      <div id="second" className="section second">
-        <h1>2nd section</h1>
-      </div>
-      <div id="third" className="section third">
-        3rd section
-      </div>
-      <div id="fourth" className="section fourth">
-        4th section
-      </div>
+        <div id="first" className="section first">
+          <h1>1st section</h1>
+        </div>
+        <div id="second" className="section second">
+          <h1>2nd section</h1>
+        </div>
+        <div id="third" className="section third">
+          3rd section
+        </div>
+        <div id="fourth" className="section fourth">
+          4th section
+        </div>
     </div>
   );
 }
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -4,25 +4,49 @@ import { NavHashLink as HashLink } from 'react-router-hash-link';
 import './Navigation.css';
 
 const Navigation = () => {
+  console.log('LJ - pathname: document.location.pathname + document.location.hash:', {pathname: document.location.pathname + document.location.hash});
   return (
     <div className="naviWrapper">
-      <HashLink className="naviItem first" smooth to="/#first" activeClassName="active">
+      <HashLink
+        className="naviItem first"
+        smooth
+        to="/#first"
+        activeClassName="active"
+        location={{pathname: document.location.pathname + document.location.hash}}
+      >
         FIRST
       </HashLink>
 
-      <HashLink className="naviItem second" smooth to="/#second" activeClassName="active">
+      <HashLink
+        className="naviItem second"
+        smooth to="/#second"
+        activeClassName="active"
+        location={{pathname: document.location.pathname + document.location.hash}}
+      >
         SECOND
       </HashLink>
 
-      <HashLink className="naviItem third" smooth to="/#third" activeClassName="active">
+      <HashLink
+        className="naviItem third"
+        smooth
+        to="/#third"
+        activeClassName="active"
+        location={{pathname: document.location.pathname + document.location.hash}}
+      >
         THIRD
       </HashLink>
 
-      <HashLink className="naviItem fourth" smooth to="/#fourth" activeClassName="active">
+      <HashLink
+        className="naviItem fourth"
+        smooth
+        to="/#fourth"
+        activeClassName="active"
+        location={{pathname: document.location.pathname + document.location.hash}}
+      >
         FOURTH
       </HashLink>
     </div>
   )
-}
+};
 
 export default Navigation;
