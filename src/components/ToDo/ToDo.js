c29990d4728267f4ff41b3996dabb776114f4173
julou <louis.jungo@gmail.com>
2020-01-15 17:11:20 +0100

    drawer styling and rellax

---
+++ src/components/ToDo/ToDo.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Rellax from 'react-rellax';
+
+const ToDo = ({}) => (
+  <div>
+		<h1>2nd section</h1>
+		<Rellax as="div" speed={-1} className="adsf">this one goes slow</Rellax>
+	</div>
+);
+
+ToDo.propTypes = {};
+ToDo.defaultProps = {};
+ToDo.displayName = '';
+
+export default ToDo
