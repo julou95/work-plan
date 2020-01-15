6cc21af51bd174a02018cba58b768d9687f9dbe6
julou <louis.jungo@gmail.com>
2020-01-15 17:17:13 +0100

    Updates

---
+++ precache-manifest.07bb039b1bda60a69de201409ed680ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01fe3b243199ab73b2a8a74a02e6c1d1",
+    "url": "/julou/index.html"
+  },
+  {
+    "revision": "28675efa706bb99541fe",
+    "url": "/julou/static/css/main.4b0a04ea.chunk.css"
+  },
+  {
+    "revision": "0691cd9e12c48b8913c0",
+    "url": "/julou/static/js/2.511030c3.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/julou/static/js/2.511030c3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "28675efa706bb99541fe",
+    "url": "/julou/static/js/main.ef110a1f.chunk.js"
+  },
+  {
+    "revision": "41ab8b077ed4099db27b",
+    "url": "/julou/static/js/runtime-main.57a2a6f0.js"
+  }
+]);
