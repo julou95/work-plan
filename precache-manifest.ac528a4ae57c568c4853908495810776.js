a59957a06afe4d74ca9280c160fe295074f3597f
Louis Jungo <louis.jungo@gmail.com>
2020-03-11 17:07:40 +0100

    Updates

---
+++ precache-manifest.ac528a4ae57c568c4853908495810776.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74fbd620d4b7d7c447bd023e34020fc8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61ad2bd51a7fa0a42a4d",
+    "url": "/static/css/main.fef55a80.chunk.css"
+  },
+  {
+    "revision": "26c2458d98a332c865b1",
+    "url": "/static/js/2.50dcbcc4.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.50dcbcc4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "61ad2bd51a7fa0a42a4d",
+    "url": "/static/js/main.5e100ad2.chunk.js"
+  },
+  {
+    "revision": "fe5957780b7ee066cb8a",
+    "url": "/static/js/runtime-main.7e514226.js"
+  }
+]);
